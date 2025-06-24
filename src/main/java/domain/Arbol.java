afb80ad194b1a79b58c7169989925764f58928d3
Kendall <kendallgomezmolina16@gmail.com>
2025-06-24 13:11:04 -0600

    Arbol Class

--- src/main/java/domain/Arbol.java
+++ src/main/java/domain/Arbol.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package domain;
 
-/**
- *
- * @author kgomezm
- */
-public class Arbol {
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="Arbol")
+public class Arbol implements Serializable {
     
+    private static final long serialVersionUID = 1L;
+    
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idArbol;
+    
+    private String nombreComun;
+    private String tipoFlor;
+    private double durezaMadera;
+    private double alturaPromedio;
+    private String rutaImg;
+
 }
